d86afe78aeb46a9b65b10d2c5d9c24d998571716
ShaneKolkoto <shanekolkoto@gmail.com>
2022-05-10 13:19:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# Disney Plus Clone
+
+This Repo clone is very similar to the original disney plus website. It is only one page(homepage) website. It has navbar and search box with cool click effect same as disney+ and it also has a slider or carousel with infinity or endless effect. Which was very hard for me to make at first. And after that we also have movie cards. With awesome card hover effect.
+
+
+## Authors
+
+- [@ShaneKolkoto](https://github.com/ShaneKolkoto)
+
+
+## Tech Stack
+
+**Client:** HTML, CSS, Javascript
+
+
+## Screenshots
+
+![App Screenshot](./assets/img/preview.png)
+
---
+++ scripts/data.js
@@ -0,0 +1,18 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: './assets/img/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: './assets/img/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: './assets/img/slider 3.PNG'
+    },
+/// and so on
+]
